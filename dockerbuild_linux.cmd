94be059a30ca89ade8c3a2acaf181baa2d1d6c56
Richard Case <richard.case@outlook.com>
2017-08-15 10:39:08 +0100

    Added linux stuff

---
+++ dockerbuild_linux.cmd
@@ -0,0 +1,13 @@
+REM Migrate this to cake
+
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
+set VAR_DATE=%Year%-%Month%-%Day%-%Hour%-%Minute%-%Second% 
+@set VAR_VER=0.0.0
+for /f %%a in ('git rev-parse --short HEAD') do @set VAR_REF=%%a
+
+dotnet clean
+dotnet restore
+dotnet publish containertest.csproj -c Release -r debian.8-x64
+docker build -f Dockerfile_linux -t richardcase/containertest-api_linux --build-arg BUILD_DATE=%VAR_DATE% --build-arg VCS_REF=%VAR_REF% --build-arg VERSION=%VAR_VER%  .
+
+ECHO Done
